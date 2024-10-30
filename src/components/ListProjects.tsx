f285d9dbc85372d8bfa2e83a1f7c787616434b1a
JaskiratAnand <jaskiratanand01@gmail.com>
2024-10-30 23:24:46 +0530

    improving accessability

--- src/components/ListProjects.tsx
+++ src/components/ListProjects.tsx
@@ -20,18 +20,18 @@ const ListProjects = memo(() => {
             ))}
           </div>
 
-          <div className="flex flex-row gap-4 text-sm font-medium mt-3 text-blue-500">
+          <div className="flex flex-row gap-4 text-sm font-medium mt-3 text-blue-800 dark:text-blue-300">
             <a
               href={project.github_repo}
               target="_blank"
-              className="h-7 border flex flex-col justify-center items-center px-2 py-1 rounded-full bg-neutral-100 dark:bg-neutral-700 dark:border-neutral-700 hover:bg-neutral-300 dark:hover:bg-neutral-800"
+              className="h-7 border flex flex-col justify-center items-center px-2 py-1 rounded-full bg-neutral-100 dark:bg-neutral-700 dark:border-neutral-800 hover:bg-neutral-300 dark:hover:bg-neutral-900"
             >GitHub Repo</a>
 
             {project.live_link &&
               <a
                 href={project.live_link}
                 target="_blank"
-                className="h-7 border flex flex-col justify-center items-center px-2 py-1 rounded-full bg-neutral-100 dark:bg-neutral-700 dark:border-neutral-700 hover:bg-neutral-300 dark:hover:bg-neutral-800"
+                className="h-7 border flex flex-col justify-center items-center px-2 py-1 rounded-full bg-neutral-100 dark:bg-neutral-700 dark:border-neutral-800 hover:bg-neutral-300 dark:hover:bg-neutral-900"
               >Live Project</a>
             }
           </div>
